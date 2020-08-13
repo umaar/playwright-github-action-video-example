09147c990d207e156f40415a8f823a5aeab04304
Umar Hansa <umar.hansa@gmail.com>
2020-08-13 12:11:34 +0100

    updates test

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import playwrightVideo from 'playwright-video';
 
 const {mkdir} = fsPromise;
 
-function sleep(ms = 1000) {return new Promise((resolve) => setTimeout(resolve, ms))};
-
 const artifactsFolder = 'test-output'
 
 await mkdir(artifactsFolder, {recursive: true});
@@ -13,19 +11,18 @@ await mkdir(artifactsFolder, {recursive: true});
 console.log('Launching Chrome');
 
 const browser = await chromium.launch({
-	headless: true
+	headless: false,
+	slowMo: 100
 });
 const context = await browser.newContext();
 const page = await context.newPage();
 await playwrightVideo.saveVideo(page, `${artifactsFolder}/video.mp4`);
 
-await page.goto('https://example.com');
-
-await sleep();
-
-await page.screenshot({
-	path: `${artifactsFolder}/screenshot.png`
-});
-
-await sleep();
+await page.goto('https://smashingconf.com/online-workshops/');
+await page.click(`[href$="/speakers"]`);
+await page.click(`[href$="/workshops"]`);
+await page.click(`[href$="/software"]`);
+await page.click(`[href$="/talks"]`);
 await browser.close();
+
+console.log('Closing browser');
