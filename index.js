696b2f7bcdefa5308c6c759fceae7db2c72845e4
Umar Hansa <umar.hansa@gmail.com>
2020-08-13 12:25:09 +0100

    use `finally` to close browser and stop vid, set timeout

--- index.js
+++ index.js
@@ -14,15 +14,21 @@ const browser = await chromium.launch({
 	headless: true,
 	slowMo: 100
 });
+
 const context = await browser.newContext();
 const page = await context.newPage();
-await playwrightVideo.saveVideo(page, `${artifactsFolder}/video.mp4`);
+page.setDefaultTimeout(5000);
 
-await page.goto('https://smashingconf.com/online-workshops/');
-await page.click(`[href$="/speakers"]`);
-await page.click(`[href$="/workshops"]`);
-await page.click(`[href$="/software"]`);
-await page.click(`[href$="/talks"]`);
-await browser.close();
+const capture = await playwrightVideo.saveVideo(page, `${artifactsFolder}/video.mp4`);
 
-console.log('Closing browser');
+try {
+	await page.goto('https://smashingconf.com/online-workshops/');
+	await page.click(`[href$="/speakers"]`);
+	await page.click(`[href$="/workshops"]`);
+	await page.click(`[href$="/software"]`);
+	await page.click(`[href$="/talks"]`);	
+} finally {
+	await capture.stop();
+	await browser.close();
+	console.log('Closed browser');
+}
